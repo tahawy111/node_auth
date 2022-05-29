790c8f0e7a32452d6fb4690fd57def76a00147d1
tahawy7 <amer.zp582@gmail.com>
2022-05-29 17:43:11 +0200

    .

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
+// App Sittings
 const express = require("express");
 const app = express();
-const port = 3000;
-const mongoose = require("mongoose");
-app.use(express.json());
+const PORT = process.env.PORT || 5000;
+const expressLayouts = require("express-ejs-layouts");
+// app sittings
+
+// Passport Sittings
+
+// Middlewares
+
+// Routes
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
+
+// EJS
+app.use(expressLayouts);
 app.set("view engine", "ejs");
+app.use(express.json());
 
 // Static Folders
 app.use(express.static("public"));
 
-// Router
-app.use(require("./routes/router"));
+// End Middlewares
 
 // Start Server
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
