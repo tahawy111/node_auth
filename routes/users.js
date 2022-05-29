05172c2e73e1d79ab39659973ce04bf434c6bb85
TAHAWY <70866079+tahawy111@users.noreply.github.com>
2022-05-30 00:39:26 +0200

    complete register

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 // Login  Page
 router.get("/login", (req, res) => {
@@ -30,8 +32,38 @@ router.post("/register", (req, res) => {
     res.render("register", { errors, name, email, password, password2 });
   } else {
     // Validation Passed
+
+    User.findOne({ email: email }).then((user) => {
+      // User exists
+      if (user) {
+        errors.push({ msg: "Email is already registerd" });
+        res.render("register", { errors, name, email, password, password2 });
+      } else {
+        const newUser = new User({
+          name: name,
+          email: email,
+          password: password,
+        });
+
+        // Hash password
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+
+            // Set password hashed
+            newUser.password = hash;
+
+            newUser
+              .save()
+              .then(() => {
+                res.redirect("/users/login");
+              })
+              .catch((err) => console.log(err));
+          })
+        );
+      }
+    });
   }
-  console.log(errors);
 });
 
 module.exports = router;
