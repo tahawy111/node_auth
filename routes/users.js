fd7b00722d6e126f6029af6f0411359339169797
TAHAWY <70866079+tahawy111@users.noreply.github.com>
2022-05-29 23:26:23 +0200

    .

--- routes/users.js
+++ routes/users.js
@@ -29,7 +29,7 @@ router.post("/register", (req, res) => {
   if (errors.length > 0) {
     res.render("register", { errors, name, email, password, password2 });
   } else {
-    res.send("pass");
+    // Validation Passed
   }
   console.log(errors);
 });
